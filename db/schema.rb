81c7bb3530d71c768fd79995bb8590a49ad25e5d
Rémy Hannequin <remy@thoughtbot.com>
2023-10-13 13:52:37 +0200

    [MIG] Create planets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_06_123141) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_13_114527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "planets", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_planets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -26,4 +33,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_06_123141) do
     t.index ["remember_token"], name: "index_users_on_remember_token", unique: true
   end
 
+  add_foreign_key "planets", "users"
 end
