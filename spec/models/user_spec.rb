dda2bd027115f2dcfdd7fc4c18658261c4095446
Rémy Hannequin <remy@thoughtbot.com>
2023-10-13 16:13:15 +0200

    Improve database consistency for model user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,25 @@
 
 require "rails_helper"
 
-describe User do
+describe User, type: :model do
   describe "factory" do
     it "allows to create a records" do
       expect { create(:user) }.not_to raise_error
     end
   end
 
-  it { is_expected.to validate_presence_of(:email) }
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:encrypted_password) }
+
+    it "validates uniqueness of email" do
+      create(:user, email: "example@email.com")
+      user2 = build(:user, email: "example@email.com")
+
+      expect(user2).not_to be_valid
+      expect(user2.errors.full_messages).to(
+        include "Email has already been taken"
+      )
+    end
+  end
 end
