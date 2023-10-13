cd6b37b59f27752f85485d56b68c97a9f4883469
Rémy Hannequin <remy@thoughtbot.com>
2023-10-13 16:13:15 +0200

    Improve database consistency for model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@
 
 class User < ApplicationRecord
   include Clearance::User
+
+  validates :confirmation_token, length: {maximum: 128}, uniqueness: true
+  validates :encrypted_password, presence: true, length: {maximum: 128}
 end
