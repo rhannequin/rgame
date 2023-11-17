4906394af867580c7b4469d88e9d27f2ac65d5a9
Rémy Hannequin <remy@thoughtbot.com>
2023-11-17 16:21:40 +0100

    Define costs and duration for metal mine upgrades

--- app/models/metal_mine_upgrade.rb
+++ app/models/metal_mine_upgrade.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
 class MetalMineUpgrade < ApplicationRecord
+  BASE_COST = 60
+  CUMULATIVE_FACTOR = 4.5
+  TIME_FACTOR = 7.0
+
   belongs_to :planet
 
   validates :target_level, presence: true, numericality: {only_integer: true}
   validates :ends_at, presence: true
+
+  def self.cost_for_level(level)
+    (BASE_COST * CUMULATIVE_FACTOR**(level - 1)).floor
+  end
+
+  def self.duration_for_level(level)
+    (cost_for_level(level) / TIME_FACTOR).round.seconds
+  end
 end
--- spec/models/metal_mine_upgrade_spec.rb
+++ spec/models/metal_mine_upgrade_spec.rb
@@ -18,4 +18,34 @@ describe MetalMineUpgrade do
     it { is_expected.to validate_numericality_of(:target_level).only_integer }
     it { is_expected.to validate_presence_of(:ends_at) }
   end
+
+  describe "::cost_for_level" do
+    [
+      {level: 2, cost: 270},
+      {level: 3, cost: 1_215},
+      {level: 4, cost: 5_467},
+      {level: 5, cost: 24_603},
+      {level: 6, cost: 110_716}
+    ].each do |test_case|
+      it "returns the right cost for the level #{test_case[:level]}" do
+        cost = described_class.cost_for_level(test_case[:level])
+        expect(cost).to eq(test_case[:cost])
+      end
+    end
+  end
+
+  describe "::duration_for_level" do
+    [
+      {level: 2, duration: 39.seconds},
+      {level: 3, duration: 174.seconds},
+      {level: 4, duration: 781.seconds},
+      {level: 5, duration: 3_515.seconds},
+      {level: 6, duration: 15_817.seconds}
+    ].each do |test_case|
+      it "returns the right duration for the level #{test_case[:level]}" do
+        duration = described_class.duration_for_level(test_case[:level])
+        expect(duration).to eq(test_case[:duration])
+      end
+    end
+  end
 end
