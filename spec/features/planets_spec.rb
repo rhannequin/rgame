ee072943e8077d34b195b5f2f19511fd62362a42
Rémy Hannequin <remy@thoughtbot.com>
2023-11-18 00:45:43 +0100

    Cover first part of upgrade process with feature specs

--- spec/features/planets_spec.rb
+++ spec/features/planets_spec.rb
@@ -27,4 +27,26 @@ describe "Planets" do
       end
     end
   end
+
+  describe "planet's metal mine upgrades" do
+    it "enables the user to upgrade the metal mine level" do
+      travel_to Time.zone.local(2023, 10, 22, 12, 30, 0) do
+        user = create(:user, password: "password")
+        create(:planet, user: user, metal_mine_level: 1, metal: 10_000)
+        sign_in_with(user.email, "password")
+
+        visit root_path
+        click_link I18n.t("planets.homeworld")
+
+        expect(page).to have_content("Upgrade")
+        expect(page).to have_content("270 Metal")
+        expect(page).to have_content("39 seconds")
+
+        click_link "Upgrade"
+
+        expect(page).to have_content("9,730")
+        expect(page).to have_content("Upgrade to level 2 finished in 39 seconds")
+      end
+    end
+  end
 end
