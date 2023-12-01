736f7ac789604bf498fdb5732e055e7753b04ddd
Rémy Hannequin <remy@thoughtbot.com>
2023-12-01 12:21:31 +0100

    Fix Tailwind classes in flash message helper

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -3,11 +3,11 @@
 module FlashHelper
   def color_for_alert_type(type)
     case type
-    when "notice" then :green
-    when "info" then :blue
-    when "warning" then :orange
-    when "error" then :red
-    else :blue
+    when "notice" then "border-green-400 text-green-700"
+    when "info" then "border-blue-400 text-blue-700"
+    when "warning" then "border-orange-400 text-orange-700"
+    when "error" then "border-red-400 text-red-700"
+    else "border-blue-400 text-blue-700"
     end
   end
 end
--- spec/helpers/flash_helper_spec.rb
+++ spec/helpers/flash_helper_spec.rb
@@ -5,23 +5,33 @@ require "rails_helper"
 describe FlashHelper do
   describe "#color_for_alert_type" do
     it "returns the correct color for notice" do
-      expect(helper.color_for_alert_type("notice")).to eq(:green)
+      expect(helper.color_for_alert_type("notice")).to(
+        eq("border-green-400 text-green-700")
+      )
     end
 
     it "returns the correct color for info" do
-      expect(helper.color_for_alert_type("info")).to eq(:blue)
+      expect(helper.color_for_alert_type("info")).to(
+        eq("border-blue-400 text-blue-700")
+      )
     end
 
     it "returns the correct color for warning" do
-      expect(helper.color_for_alert_type("warning")).to eq(:orange)
+      expect(helper.color_for_alert_type("warning")).to(
+        eq("border-orange-400 text-orange-700")
+      )
     end
 
     it "returns the correct color for error" do
-      expect(helper.color_for_alert_type("error")).to eq(:red)
+      expect(helper.color_for_alert_type("error")).to(
+        eq("border-red-400 text-red-700")
+      )
     end
 
     it "returns the correct color for unknown type" do
-      expect(helper.color_for_alert_type("unknown")).to eq(:blue)
+      expect(helper.color_for_alert_type("unknown")).to(
+        eq("border-blue-400 text-blue-700")
+      )
     end
   end
 end
